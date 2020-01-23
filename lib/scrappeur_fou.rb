c07fcfd3d6b5148fc8f6f86109d6bf806a153e6c
Maxime Besse <bessemaxime@gmail.com>
2020-01-23 20:22:23 +0100

    end crypto 2

--- lib/scrappeur_fou.rb
+++ lib/scrappeur_fou.rb
@@ -1,36 +1,30 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-PAGE_URL = "https://coinmarketcap.com/"
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 
 
 
 def get_crypto_name_from_html
 page = Nokogiri::HTML(open(PAGE_URL))
-array_title=[]
-
-html_title = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr//td[2]//a/@title')
-
-
-array_title = html_title.to_a
-
-  return array_title
+array_all_crypto_names  = []
+  page.xpath("//tr/td[3]/div").each do |name|
+  name.text
+  #sleep(0.1) #=> décommenter les 2 lignes pour faire de l'affichage 1 par 1
+  array_all_crypto_names << name.text
+  end
+  return array_all_crypto_names
 end
-
-
 
 
 def get_crypto_value_from_html
   page = Nokogiri::HTML(open(PAGE_URL))
 
-  array_value=[]
-
-  html_value = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr//td[4]/a/text()')
-
-  array_value = html_value.to_a
-  array_value = html_value.map { |value| value.to_s.delete("$").to_f}
-
-  return array_value
+  array_all_crypto_values = []
+  page.xpath("//tr/td[5]/a").each do |value|
+  array_all_crypto_values << value.text.delete("$,")
+  end
+  return array_all_crypto_values
 end
 
 
@@ -40,29 +34,18 @@ def put_arrays_into_hash
   results = get_crypto_name_from_html.zip(get_crypto_value_from_html)
 
   return results
+
 end
 
-def put_results_into_array
+def put_hash_into_result
 
   result_final = []
-
-put_arrays_into_hash.each do |result|
-
+  put_arrays_into_hash.each do |result|
   hash_temp = Hash.new
   hash_temp[result[0]]=result[1]
   result_final << hash_temp
-  # en une ligne : result_final << {result[0] => result[1]}
-  end
-
-  return result_final
-
 end
-
-def perform
-
-  puts put_results_into_array
-
 end
 
 
-perform
+puts put_hash_into_result
--- lib/test.rb
+++ lib/test.rb
@@ -1,13 +1,31 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-PAGE_URL = "https://coinmarketcap.com/"
-
-
-
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 page = Nokogiri::HTML(open(PAGE_URL))
-array_title=[]
-
-html_title = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr//td[2]//a/@title')
-
-puts html_title
+#RECUPERATION DES NOMS
+array_all_crypto_names  = []
+page.xpath("//tr/td[3]/div").each do |name|
+  #puts name.text
+  #sleep(0.1) => décommenter les 2 lignes pour faire de l'affichage 1 par 1
+  array_all_crypto_names << name.text
+end
+#RECUPERATION DES VALEURS
+array_all_crypto_values  = []
+page.xpath("//tr/td[5]/a").each do |value|
+  #puts value.text.delete("$,")
+  #sleep(0.1) => décommenter les 2 lignes pour faire de l'affichage 1 par 1
+  array_all_crypto_values << value.text.delete("$,")
+end
+#MISE EN FORME DU RESULTAT
+results = Hash.new
+results = array_all_crypto_names.zip(array_all_crypto_values)
+#puts "#{results}"
+result_final = []
+results.each do |result|
+  hash_temp = Hash.new
+  hash_temp[result[0]]=result[1]
+  result_final << hash_temp
+  # en une ligne : result_final << {result[0] => result[1]}
+end
+puts result_final
