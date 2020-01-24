0f20e70d39803bacb19e58db3aaa67249dafd836
Maxime Besse <bessemaxime@gmail.com>
2020-01-24 09:20:32 +0100

    end crypto 2

--- lib/scrappeur_fou.rb
+++ lib/scrappeur_fou.rb
@@ -10,11 +10,11 @@ page = Nokogiri::HTML(open(PAGE_URL))
 array_all_crypto_names  = []
   page.xpath("//tr/td[3]/div").each do |name|
   name.text
-  #sleep(0.1) #=> décommenter les 2 lignes pour faire de l'affichage 1 par 1
   array_all_crypto_names << name.text
   end
   return array_all_crypto_names
 end
+
 
 
 def get_crypto_value_from_html
@@ -26,6 +26,7 @@ def get_crypto_value_from_html
   end
   return array_all_crypto_values
 end
+
 
 
 def put_arrays_into_hash
@@ -45,7 +46,7 @@ def put_hash_into_result
   hash_temp[result[0]]=result[1]
   result_final << hash_temp
 end
+return result_final
 end
-
 
 puts put_hash_into_result
